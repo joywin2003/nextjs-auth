ffec5612746e1c112048fca5e068995869eb7426
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-03-16 14:34:41 +0530

    auth-set

--- src/app/resetpassword/page.tsx
+++ src/app/resetpassword/page.tsx
@@ -20,7 +20,6 @@ export default function resetPassword() {
     handleSubmit,
     formState: { errors, isSubmitting },
     reset,
-    getValues,
   } = useForm<TResetpasswordSchema>({
     resolver: zodResolver(resetpasswordSchema),
   });
@@ -29,6 +28,7 @@ export default function resetPassword() {
 
   const resetPassword = async (data: TResetpasswordSchema) => {
     try {
+      const toastId = toast.loading("Resetting password...");
       const response = await axios.patch("/api/users/resetpassword", {
         password: data.password,
         token,
@@ -36,6 +36,8 @@ export default function resetPassword() {
       console.log(response.data);
       await new Promise<void>((resolve) =>
         setTimeout(() => {
+          toast.dismiss(toastId);
+          toast.success("Password reset successful");
           router.push("/");
           resolve();
         }, 2000)
@@ -65,10 +67,11 @@ export default function resetPassword() {
             placeholder="Password"
             type="password"
           />
+          {errors.password && (
+            <p className="text-red-500">{`${errors.password.message}`}</p>
+          )}
         </LabelInputContainer>
-        {errors.password && (
-          <p className="text-red-500">{`${errors.password.message}`}</p>
-        )}
+
         <LabelInputContainer className="mb-8">
           <Label htmlFor="twitterpassword">Confirm Password</Label>
           <Input
@@ -76,10 +79,10 @@ export default function resetPassword() {
             placeholder="Confirm Password"
             type="password"
           />
+          {errors.confirmpassword && (
+            <p className="text-red-500">{`${errors.confirmpassword.message}`}</p>
+          )}
         </LabelInputContainer>
-        {errors.confirmpassword && (
-          <p className="text-red-500">{`${errors.confirmpassword.message}`}</p>
-        )}
         <button
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
